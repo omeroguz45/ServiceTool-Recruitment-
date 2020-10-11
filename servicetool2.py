34c8b845c52d3b19d74add456ddaf188c5f0b5df
omeroguz45 <omeroguz.ozk@gmail.com>
2020-10-11 20:27:17 +0300

    v2.3

--- servicetool2.py
+++ servicetool2.py
@@ -19,44 +19,45 @@ def servicestat(service):
 
 
 def main():
-    #creating the main command line interface
-    screen = curses.initscr() #initialize screen
-    num_rows, num_cols = screen.getmaxyx() #get the size of the terminal that the program runs on
-    half_col = int(round(num_cols/2))
-    
-    #creating an output window where services and their statuses are shown
-    output_win = curses.newwin(num_rows-1, half_col+5, 0, 0)
-    output_win.addstr(0,0, f'ServiceTool Version {version}, made by omeroguz45.')
-    
-    #opening and reading the services from services.txt
-    servicesfile = open('./services.txt', 'r')
-    services = [i.replace('\n', '') for i in servicesfile.readlines()]
-    r = 2 #row number for where to print the service name and status
-    #printing every service and their status on the output window
-    for service in services:
-        output_win.addstr(r,0, f'{service} - {servicestat(service)}')
-        r += 1
-    for line in range(0, num_rows-2):
-        output_win.addstr(line, half_col+4, '|')
-    #showing the output window
-    output_win.refresh()
+    while True:
+        #creating the main command line interface
+        screen = curses.initscr() #initialize screen
+        num_rows, num_cols = screen.getmaxyx() #get the size of the terminal that the program runs on
+        half_col = int(round(num_cols/2))
 
-    input_win = curses.newwin(2,num_cols,num_rows-2,0)
-    for column in range(0, num_cols):
-        input_win.addstr(0, column, '-')
-    command = get_input(input_win, 1, 0)
-    command = command.decode('utf-8')
-    curses.flushinp()
-    curses.setsyx(1,1)
-    input_win.clrtoeol()
+        #creating an output window where services and their statuses are shown
+        output_win = curses.newwin(num_rows-1, half_col+5, 0, 0)
+        output_win.addstr(0,0, f'ServiceTool Version {version}, made by omeroguz45.')
 
-    info_win = curses.newwin(num_rows-1, half_col-5, 0, half_col+6)
-    info_win.addstr(0, 0, command)
-    info_win.refresh()
-    input_win.refresh()
+        #opening and reading the services from services.txt
+        servicesfile = open('./services.txt', 'r')
+        services = [i.replace('\n', '') for i in servicesfile.readlines()]
+        r = 2 #row number for where to print the service name and status
+        #printing every service and their status on the output window
+        for service in services:
+            output_win.addstr(r,0, f'{service} - {servicestat(service)}')
+            r += 1
+        for line in range(0, num_rows-2):
+            output_win.addstr(line, half_col+4, '|')
+        #showing the output window
+        output_win.refresh()
 
-    curses.napms(200000)
-    curses.endwin()
+        input_win = curses.newwin(2,num_cols,num_rows-2,0)
+        for column in range(0, num_cols):
+            input_win.addstr(0, column, '-')
+        command = get_input(input_win, 1, 0)
+        command = command.decode('utf-8')
+        curses.flushinp()
+        curses.setsyx(1,1)
+        input_win.clrtoeol()
+
+        info_win = curses.newwin(num_rows-1, half_col-5, 0, half_col+6)
+        info_win.addstr(0, 0, command)
+        info_win.refresh()
+        input_win.refresh()
+
+        curses.napms(200000)
+        curses.endwin()
 
 if __name__ == '__main__':
     main()
