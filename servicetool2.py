64ea75ecb4fe3ae43d1d4bce269ea99f64316943
omeroguz45 <omeroguz.ozk@gmail.com>
2020-10-11 13:47:50 +0300

    v2.3

--- servicetool2.py
+++ servicetool2.py
@@ -7,16 +7,15 @@ def servicestat(service):
     p = subprocess.Popen(['systemctl', 'is-active', str(service)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out = p.stdout.read()
     out = out.decode('utf-8')
-    sys.stdout.write(out + '\r')
-    sys.stdout.flush()
+    return (service, out)
 
 
 
 def main():
     print(f'ServiceTool, Version{version} by omeroguz45')
     while True:
-        servicestat('ssh')
-        servicestat('firewall')
+        print(servicestat('ssh'), end='\r')
+        print(servicestat('firewall'), end='\r')
 
 if __name__ == '__main__':
     main()
