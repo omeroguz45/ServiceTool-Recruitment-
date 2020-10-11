d22c4e057c1f0c1a59f5a6901be15e5c304c7869
omeroguz45 <omeroguz.ozk@gmail.com>
2020-10-11 11:57:31 +0300

    v2.0

---
+++ servicetool2.py
@@ -0,0 +1,20 @@
+import subprocess
+
+version = 1.0
+
+def servicestat(service):
+    p = subprocess.Popen(['systemctl', 'is-active', str(service)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    return out
+
+
+
+def main():
+    print(f'ServiceTool, Version{version} by omeroguz45')
+    while True:
+        service = 'ssh'
+        stat = servicestat(service)
+        print('{service} - {stat}', end='\r')
+
+if __name__ == '__main__':
+    main()
